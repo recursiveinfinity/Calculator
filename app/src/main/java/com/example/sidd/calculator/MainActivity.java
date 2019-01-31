d85c4b2f00d5d147a2b32bc32d27218f2136b01f
Sidd AR <user@example.com>
2019-01-31 12:19:52 +0000

    Implemented Add function

--- app/src/main/java/com/example/sidd/calculator/MainActivity.java
+++ app/src/main/java/com/example/sidd/calculator/MainActivity.java
@@ -1,15 +1,55 @@
 package com.example.sidd.calculator;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNumberOne;
+    EditText etNumberTwo;
+
+    Button btnAdd;
+
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String message = "Hello";
+        etNumberOne = findViewById(R.id.etNumberOne);
+        etNumberTwo = findViewById(R.id.etNumberTwo);
+
+        btnAdd = findViewById(R.id.btnAdd);
+
+        tvResult = findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = add(etNumberOne.getText().toString(),
+                        etNumberTwo.getText().toString());
+                tvResult.setText(result);
+            }
+        });
+
+    }
+
+    private String add(String numberOne, String numberTwo) {
+        if (numberOne.equals("") || numberTwo.isEmpty()) {
+            Toast.makeText(this, "Please enter a valid number",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        int a = Integer.parseInt(numberOne);
+        int b = Integer.parseInt(numberTwo);
+        int result = a + b;
+        return Integer.toString(result);
     }
 }
