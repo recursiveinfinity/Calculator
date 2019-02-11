c132b6214fdec7fa67242dfda63fd8a1c2793d1c
Sidd AR <user@example.com>
2019-02-11 18:12:44 +0000

    Added Permissions

--- app/src/main/java/com/example/sidd/calculator/MainActivity.java
+++ app/src/main/java/com/example/sidd/calculator/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.sidd.calculator;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.constraint.Group;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +19,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Group nameGroup;
 
     private List<String> log = new ArrayList<>();
+
+    private boolean isPermissionRequestInProgress = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void showLogs() {
         if (log.size() > 0) {
-            writeLogsToFile();
+            writeLogsToExternalStorage();
         }
         Intent intent = new Intent(MainActivity.this,
                 LogsActivity.class);
@@ -114,7 +121,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         intent.putExtra("Bus", bus);
         Room room = new Room("Training Room", 4);
         intent.putExtra("Room", room);
-        startActivity(intent);
+        if(!isPermissionRequestInProgress) {
+            startActivity(intent);
+        }
     }
 
     private void readNameFromSharedPreferences() {
@@ -128,8 +137,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void writeLogsToFile() {
-        File file = new File(getFilesDir(), "Logs.txt");
-        //FileOutputStream fileOutputStream = null;
         try(FileOutputStream fileOutputStream =
                     openFileOutput("Logs.txt", Context.MODE_PRIVATE)){
             StringBuilder stringBuilder = new StringBuilder();
@@ -143,6 +150,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } catch(IOException ioException) {
             Toast.makeText(this, "File Not Found", Toast.LENGTH_SHORT).show();
             ioException.printStackTrace();
+        }
+    }
+
+    private void writeLogsToExternalStorage() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestStoragePermission();
+        } else {
+            writeFile();
+        }
+    }
+
+    private void requestStoragePermission() {
+        isPermissionRequestInProgress = true;
+        ActivityCompat.requestPermissions(this,
+                new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            isPermissionRequestInProgress = false;
+            writeFile();
+        }
+    }
+
+    private void writeFile() {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File parentFolder = new File(Environment
+                    .getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_DOCUMENTS), "Calculator");
+            parentFolder.mkdirs();
+
+            File file = new File(parentFolder, "Logs.txt");
+
+            try(FileOutputStream fileOutputStream =
+                        new FileOutputStream(file)){
+                StringBuilder stringBuilder = new StringBuilder();
+                for (String result : log) {
+                    stringBuilder.append(result);
+                    stringBuilder.append("\n");
+                }
+                fileOutputStream.write(stringBuilder.toString().getBytes());
+
+
+            } catch(IOException ioException) {
+                Toast.makeText(this, "File Not Found", Toast.LENGTH_SHORT).show();
+                ioException.printStackTrace();
+            }
         }
     }
 }
